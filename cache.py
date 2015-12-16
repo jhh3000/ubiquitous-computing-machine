68e945049f5002b76bc08751741d68c36ecd33e7
Jeff Hu <jhh3000@gmail.com>
2015-12-16 17:01:14 -0500

    cache updates

---
+++ cache.py
@@ -0,0 +1,52 @@
+import datetime
+import time
+import urllib2
+import json
+
+
+CACHE_FILE = "cache.html"
+URL_LONG_IDEAS = "http://seekingalpha.com/analysis/investing-ideas/long-ideas/%s"
+
+class Cache(object):
+	"""
+	Handles retrieval and update of the Seeking Alpha lists
+	"""
+
+	def __init__(self):
+		pass
+		
+	def update(self):
+		f = open(CACHE_FILE, 'w')
+
+		url = URL_LONG_IDEAS % now
+		user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+		headers = { 'User-Agent' : user_agent }
+
+		req = urllib2.Request(url, headers=headers)
+
+		now = datetime.datetime.now()
+		now = time.mktime(now.timetuple())
+		now = "%0.d" % now
+
+		for x in range(0,1000):
+
+			print "Capturing Date: %s" % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(now)))
+			
+			response = urllib2.urlopen(req)
+			the_page = response.read()
+
+			data = json.loads(the_page)
+
+			now = data['last_ts']
+			html_doc = data['html']
+			try:
+				f.write(html_doc)
+			except UnicodeEncodeError:
+				pass
+			f.flush()
+
+		f.close()
+
+	def get(self):
+		f = open(CACHE_FILE, 'r')
+		return f.read()
--- project.py
+++ project.py
@@ -1,37 +1,13 @@
-import datetime
-import time
-import urllib2
-import json
+from cache import Cache
+
 from bs4 import BeautifulSoup
 
-f = open("cache.html", 'w')
+cache = Cache()
+html_doc = cache.get()
 
-now = datetime.datetime.now()
-now = time.mktime(now.timetuple())
-now = "%0.d" % now
+soup = BeautifulSoup(html_doc, 'html.parser')
 
-for x in range(0,1000):
-
-	print "Capturing Date: %s" % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(now)))
-
-	url = 'http://seekingalpha.com/analysis/investing-ideas/long-ideas/%s' % now
-	user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-	headers = { 'User-Agent' : user_agent }
-
-	req = urllib2.Request(url, headers=headers)
-	response = urllib2.urlopen(req)
-	the_page = response.read()
-
-	data = json.loads(the_page)
-
-	now = data['last_ts']
-	html_doc = data['html']
-	try:
-		f.write(html_doc)
-	except UnicodeEncodeError:
-		pass
-	f.flush()
-
-#soup = BeautifulSoup(html_doc, 'html.parser')
-
-f.close()
+articles = soup.find_all('li')
+article = articles[0]
+ticker = article.span.a.text
+pub_date = article.div.div.contents[4]
