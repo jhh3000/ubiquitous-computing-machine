614ff9809be065648ee9aec80d87e08d8064c96d
Jeff Hu <jhh3000@gmail.com>
2015-12-16 23:56:25 -0500

    auto update with memory

--- cache.py
+++ cache.py
@@ -2,10 +2,31 @@ import datetime
 import time
 import urllib2
 import json
+import pickle
+import parsedatetime
+import dateutil.parser
+from time import mktime
+
+from bs4 import BeautifulSoup
 
 
-CACHE_FILE = "cache2.html"
+CACHE_FILE = "data.json"
 URL_LONG_IDEAS = "http://seekingalpha.com/analysis/investing-ideas/long-ideas/%s"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+def json_load(obj):
+	try:
+		data = json.loads(obj)
+	except (EOFError, ValueError):
+		data = {}
+	for key in data:
+		data[key] = (data[key][0], datetime.datetime.strptime(data[key][1], DATE_FORMAT))
+	return data
+
+def json_dump(data):
+	for key in data:
+		data[key] = (data[key][0], data[key][1].strftime(DATE_FORMAT))
+	return json.dumps(data, sort_keys=True, indent=2, separators=(',', ': '))
 
 class Cache(object):
 	"""
@@ -14,24 +35,80 @@ class Cache(object):
 
 	def __init__(self):
 		pass
+
+	def _parse(self, html_doc):
+		soup = BeautifulSoup(html_doc, 'html.parser')
+
+		articles = soup.find_all('li')
+
+		data = {}
+
+		for article in articles:
+			article_id = article['article_id']
+			
+			ticker = None
+			for d in article.find_all('span'):
+				if d.a:
+					ticker = d.a.text
+			if not ticker: continue
+
+			pub_date = None
+			for d in article.div.div.contents:
+				if '\n\n        ' in d:
+					pub_date = d
+			if not pub_date: continue
+
+			if 'Today' in pub_date or 'Yesterday' in pub_date:
+				cal = parsedatetime.Calendar()
+				time_struct, parse_status = cal.parse(pub_date)
+				pub_date = datetime.datetime.fromtimestamp(mktime(time_struct))
+			else:
+				pub_date = dateutil.parser.parse(pub_date)
+
+			print "%s: %s - %s" % (article_id, ticker, pub_date)
+			data[article_id] = (ticker, pub_date)
+
+		return data
+
+	def _write(self, data):
+		orig_data = self._read()
+		if not orig_data: orig_data = {}
+		orig_data.update(data)
+		f = open(CACHE_FILE, 'w')
+		f.write(json_dump(orig_data))
+		f.close()
+
+	def _read(self):
+		f = open(CACHE_FILE, 'r')
+		data = json_load(f.read())
+		f.close()
+		return data
 		
 	def update(self):
-		f = open(CACHE_FILE, 'w')
-
-		url = URL_LONG_IDEAS % now
 		user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
 		headers = { 'User-Agent' : user_agent }
 
-		req = urllib2.Request(url, headers=headers)
-
-		now = datetime.datetime.now()
+		try:
+			data = self._read()
+		except IOError:
+			data = {}
+		if data:
+			now = None
+			for key in data:
+				if not now: now = data[key][1]
+				if data[key][1] < now: now = data[key][1]
+		else:
+			now = datetime.datetime.now()
 		now = time.mktime(now.timetuple())
 		now = "%0.d" % now
 
-		for x in range(0,1000):
+		while True:
 
-			print "Capturing Date: %s" % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(now)))
+			print "Capturing Date: %s" % time.strftime(DATE_FORMAT, time.localtime(float(now)))
 			
+			url = URL_LONG_IDEAS % now
+		
+			req = urllib2.Request(url, headers=headers)
 			response = urllib2.urlopen(req)
 			the_page = response.read()
 
@@ -39,14 +116,8 @@ class Cache(object):
 
 			now = data['last_ts']
 			html_doc = data['html']
-			try:
-				f.write(html_doc)
-			except UnicodeEncodeError:
-				pass
-			f.flush()
-
-		f.close()
+			data = self._parse(html_doc)
+			self._write(data)
 
 	def get(self):
-		f = open(CACHE_FILE, 'r')
-		return f.read()
+		return self._read()
--- project.py
+++ project.py
@@ -1,50 +1,7 @@
 from cache import Cache
 
-from bs4 import BeautifulSoup
-import parsedatetime
-import dateutil.parser
-
-from datetime import datetime
-from time import mktime
-import json
-import pickle
-
 cache = Cache()
-html_doc = cache.get()
+#html_doc = cache.get()
+#cache.update()
 
-soup = BeautifulSoup(html_doc, 'html.parser')
-
-articles = soup.find_all('li')
-
-data = {}
-
-for article in articles:
-	article_id = article['article_id']
-	
-	ticker = None
-	for d in article.find_all('span'):
-		if d.a:
-			ticker = d.a.text
-	if not ticker: continue
-
-	pub_date = None
-	for d in article.div.div.contents:
-		if '\n\n        ' in d:
-			pub_date = d
-	if not pub_date: continue
-
-	if 'Today' in pub_date or 'Yesterday' in pub_date:
-		cal = parsedatetime.Calendar()
-		time_struct, parse_status = cal.parse(pub_date)
-		pub_date = datetime.fromtimestamp(mktime(time_struct))
-	else:
-		pub_date = dateutil.parser.parse(pub_date)
-
-	print "%s: %s - %s" % (article_id, ticker, pub_date)
-	data[article_id] = (ticker, pub_date)
-
-obj = pickle.dumps(data)
-
-f = open("data.txt", 'w')
-f.write(obj)
-f.close()
+data = cache.get()
