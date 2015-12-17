5c79f643ae621ff71981a5dd19c2daa9e79bd947
Jeff Hu <jhh3000@gmail.com>
2015-12-16 19:02:09 -0500

    added natural language parsing

--- project.py
+++ project.py
@@ -1,6 +1,9 @@
 from cache import Cache
 
 from bs4 import BeautifulSoup
+import parsedatetime
+from datetime import datetime
+from time import mktime
 
 cache = Cache()
 html_doc = cache.get()
@@ -11,3 +14,9 @@ articles = soup.find_all('li')
 article = articles[0]
 ticker = article.span.a.text
 pub_date = article.div.div.contents[4]
+
+cal = parsedatetime.Calendar()
+time_struct, parse_status = cal.parse(pub_date)
+
+pub_date = datetime.fromtimestamp(mktime(time_struct))
+print "%s : %s" % (ticker, pub_date)
