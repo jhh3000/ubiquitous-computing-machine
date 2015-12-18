858bce73538b78cdb4e001cc36290c617c9f9d7f
Jeff Hu <jhh3000@gmail.com>
2015-12-17 21:42:19 -0500

    renamed cache to seeking_alpha

--- project.py
+++ project.py
@@ -1,9 +1,17 @@
-from cache import update
+import seeking_alpha
 from yahoo import backtest
 
+import dateutil.parser
 
 #update()
 
-data = get()
+print "Welcome to the Ubiquitous Computing Machine."
+print "Please enter a date you'd like to test stocks from,"
+print "or leave it blank for today."
+date = raw_input("Date: ")
 
-print backtest(data[max(data)][0])
+date = dateutil.parser.parse(date)
+
+print "You chose to test stocks from: %s" % date.strftime("%b. %d, %Y")
+
+stocks = seeking_alpha.get_stocks(date)
